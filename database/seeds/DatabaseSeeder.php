08f50e00c88dbe900f23ef092eb404dd2f50d91d
fakhrikev <fakhrikev@gmail.com>
2019-03-01 17:56:20 +0700

    Add Seeder

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -11,6 +11,8 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // $this->call(UsersTableSeeder::class);
+         $this->call(product_categorySeeders::class);
+         $this->call(productSeeders::class);
+         $this->call(product_imageSeeders::class);
     }
 }
