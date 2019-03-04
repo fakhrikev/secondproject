5be38ab9cf157068cf7be617aaf2c947c611297a
fakhrikev <fakhrikev@gmail.com>
2019-03-04 16:36:12 +0700

    Menambahkan route API

--- routes/api.php
+++ routes/api.php
@@ -16,3 +16,25 @@ use Illuminate\Http\Request;
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::group(['middleware' => 'api'], function () {
+    Route::prefix('Product')->group(function () {
+        Route::get('GetAll/', 'productsController@GetAll');
+        Route::get('GetByID/{id}/', 'productsController@GetByID');
+        Route::post('Store/', 'productsController@Store');
+        Route::put('Update/{id}/', 'productsController@Update');
+        Route::delete('Delete/{id}/', 'productsController@Delete');
+    });
+
+    Route::prefix('ProductImage')->group(function () {
+        Route::get('GetWithParam/', 'productImageController@GetWithParam');
+        Route::post('Store/', 'productImageController@Store');
+        Route::put('SetMainImage/{id}/', 'productImageController@SetMainImage');
+        Route::delete('Delete/{id}/', 'productImageController@Delete');
+    });
+});
+
+
+
+
+
