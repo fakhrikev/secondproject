1b22abaf4299bf43169eebd17ed68e7e330ed7da
fakhrikev <fakhrikev@gmail.com>
2019-03-04 16:34:03 +0700

    Membuat eloquent

--- app/products.php
+++ app/products.php
@@ -3,8 +3,17 @@
 namespace App;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class products extends Model
 {
-    //
+    use SoftDeletes;
+
+     public function category(){
+         return $this->belongsTo(products_categories::class,'category_id','id');
+     }
+
+     public function  image(){
+         return $this->hasMany(products_images::class, 'product_id', 'id');
+     }
 }
