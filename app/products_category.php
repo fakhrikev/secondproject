4b89585eeacf3364d7a3eaffe32974fef63e561a
fakhrikev <fakhrikev@gmail.com>
2019-03-04 16:34:35 +0700

    Menambahkan Eloquent

--- app/products_category.php
+++ app/products_category.php
@@ -3,8 +3,13 @@
 namespace App;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class products_category extends Model
 {
-    //
+    use SoftDeletes;
+
+   public function products(){
+       return $this->hasMany(products::class, 'category_id', 'id');
+   }
 }
