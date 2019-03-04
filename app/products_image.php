57876b88791d7b8986d967846d51797d74c1f929
fakhrikev <fakhrikev@gmail.com>
2019-03-04 16:34:58 +0700

    Menambahkan Eloquent

--- app/products_image.php
+++ app/products_image.php
@@ -3,8 +3,13 @@
 namespace App;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class products_image extends Model
 {
-    //
+    use SoftDeletes;
+
+    public function products(){
+        return $this->belongsTo(products::class, 'products_id', 'id');
+    }
 }
