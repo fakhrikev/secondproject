5f19d6acf3cde1d4f0f16d92a8feee7fc755c0e9
fakhrikev <fakhrikev@gmail.com>
2019-03-08 17:42:39 +0700

    Update: Menambahkan Edit & Delete

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -2,33 +2,55 @@
 
 @section('content')
         <div>
-            <table border="1">
+            <table id="product">
                 <thead>
                 <td>Image</td>
                 <td>Product Name</td>
+                <td>Edit</td>
+                <td>Delete</td>
                 </thead>
-                <tbody id="product"></tbody>
+                <tbody></tbody>
             </table>
         </div>
 @endsection
 
 @section('script')
     <script>
+        function deleteProduct($id){
+            var r = window.confirm("Are you sure want to delete the product?");
+            if(r == true) {
+                $.ajax({
+                    url: '/api/Product/Delete/'+$id+'',
+                    type: 'DELETE',
+                    success: function () {
+                        window.location.reload();
+                    }
+                });
+            }
+            else if(r == false){
+                return null;
+            }
+        }
+
+        function editProduct($id){
+            window.location.href = '{{url("Product/Edit/")}}/'+$id;
+        }
+
         $(document).ready(function() {
             $.ajax({
                url: '/api/Product/GetAll',
                type: 'GET',
                success: function (json) {
-                   console.log(json.data[2].id);
                    $.each(json.data, function (i, product) {
-                      $('#product').append('<tr>'
-                                                + '<td>'
+                      $('#product > tbody').append('<tr><td>'
                                                 + '<img height="100px" width="100px" src = "'+ product.main_image+'">'
-                                                + '</td>'
-                                                + '<td>'
+                                                + '</td><td>'
                                                 + '<a href="{{url("Product/Detail/")}}/'+product.id+'">'+product.name+'</a>'
-                                                + '</td>'
-                                                + '</tr>');
+                                                + '</td><td>'
+                                                + '<input type="button" value="Edit" onclick="editProduct('+product.id+')">'
+                                                + '</td><td>'
+                                                + '<input type="button" value="Delete" onclick="deleteProduct('+product.id+')">'
+                                                + '</td></tr>');
                     })
                 }
             })
