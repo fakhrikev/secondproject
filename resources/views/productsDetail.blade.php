7eac40c9e80dbc7abb6c58624dbf55670e6fd1a8
fakhrikev <fakhrikev@gmail.com>
2019-03-08 17:43:04 +0700

    Update: Menampilkan gambar per produk

--- resources/views/productsDetail.blade.php
+++ resources/views/productsDetail.blade.php
@@ -4,9 +4,9 @@
     <div>
         <table id="product_detail" border="1" cellpadding="12px">
                 <thead>
+                    <td>Image</td>
                     <td>ID</td>
                     <td>SKU</td>
-                    <td>Image</td>
                     <td>Product Name</td>
                     <td>Category Name</td>
                     <td>Description</td>
@@ -23,30 +23,29 @@
     <script>
         $(document).ready(function() {
             $.ajax({
-                url: '/api/Product/GetByID/{{$id}}',
+                url: '/api/Product/GetByID/{{$id}}/',
                 type: 'GET',
                 success: function (json) {
                     var product = json.data;
                     $.ajax({
-                        url: '/api/Product/GetWithParam',
+                        url: '/api/ProductImage/GetWithParam/{{$id}}',
                         type: 'GET',
                         success: function (images) {
                             $.each(images.data, function (i, image) {
-
+                                $('#product_detail > tbody').append('<img height="100px" width="100px" src = "'+ image.product_image_url+'">');
                             });
-                        }
-
-                    });
-                        $('#product_detail > tbody').append('<tr><td>'+ product.id
+                            $('#product_detail > tbody').append(
+                                                            + '<td>' + ''
+                                                            + '</td><td>' + product.id
                                                             + '</td><td>' + product.sku
-                                                            + '</td><td>' + '<img src = "'+ product.image_url+'">'
                                                             + '</td><td>' + product.name
                                                             + '</td><td>' + product.category_name
                                                             + '</td><td>' + product.description
                                                             + '</td><td>' + product.unit_price
                                                             + '</td></tr>'
-                        );
-
+                            );
+                        }
+                    });
                 }
             })
         });
