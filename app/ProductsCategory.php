ed5c4a29a0f2835f637a00aa1878910ad300b2bd
fakhrikev <fakhrikev@gmail.com>
2019-03-12 15:41:55 +0700

    Update: penamaan

--- app/ProductsCategory.php
+++ app/ProductsCategory.php
@@ -5,7 +5,7 @@ namespace App;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\SoftDeletes;
 
-class products_category extends Model
+class productsCategory extends Model
 {
     use SoftDeletes;
 
