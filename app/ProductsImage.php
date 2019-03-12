e4b599f8d3d213dc1d4012c96ef02a0021d28504
fakhrikev <fakhrikev@gmail.com>
2019-03-12 15:43:59 +0700

    Update: penamaan

--- app/ProductsImage.php
+++ app/ProductsImage.php
@@ -5,11 +5,11 @@ namespace App;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\SoftDeletes;
 
-class products_image extends Model
+class productsImage extends Model
 {
     use SoftDeletes;
 
     public function products(){
-        return $this->belongsTo(products::class, 'products_id', 'id');
+        return $this->belongsTo(Products::class, 'products_id', 'id');
     }
 }
